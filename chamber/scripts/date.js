ada4677ddd3f7fc6c12c271d55c28e6eeddbe3e3
anthonysanderslds <asanders2@byupathway.edu>
2026-05-16 01:15:19 -0500

    performance improvements

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -2,3 +2,11 @@ const today = new Date();
 const currentYearElement = document.getElementById("currentYear");
 currentYearElement.textContent = today.getFullYear();
 document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+const navButton = document.querySelector('#ham-btn');
+const navLinks = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('open');
+    navLinks.classList.toggle('open');
+});
--- chamber/scripts/navigation.js
+++
@@ -1,7 +0,0 @@
-const navButton = document.querySelector('#ham-btn');
-const navLinks = document.querySelector('#nav-bar');
-
-navButton.addEventListener('click', () => {
-    navButton.classList.toggle('open');
-    navLinks.classList.toggle('open');
-});
